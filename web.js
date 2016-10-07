ead98401b75013541b174eefe52fd8b3a1ff1c24
Pedro Gaspar <pedro.gaspar@codistage.com>
2016-10-07 17:45:52 +0100

    build folder removed from gitignore for heroku deploy; node server to serve build folder

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by PedroGaspar on 07/10/2016.
+ */
+
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
