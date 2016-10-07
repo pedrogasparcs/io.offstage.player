6f15362e21139a0b602f6227ca7e03fd660083f8
Pedro Gaspar <pedro.gaspar@codistage.com>
2016-10-07 18:04:50 +0100

    Express Logger fixed

--- web.js
+++ web.js
@@ -3,9 +3,10 @@
  */
 
 var gzippo = require('gzippo');
+var logger = require('morgan');
 var express = require('express');
 var app = express();
 
-//app.use(express.logger('dev'));
+app.use(logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/build"));
 app.listen(process.env.PORT || 5000);
